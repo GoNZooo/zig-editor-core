56cacf75d9609bf25b55aa25a9684ccb8ec5ace6
Rickard Andersson <gonz@severnatazvezda.com>
2019-11-13 10:32:11 +0200

    add `.count` member
    Used to determine how much is actually used in the `String`

--- src/string.zig
+++ src/string.zig
@@ -6,11 +6,9 @@ const debug = std.debug;
 const mem = std.mem;
 const fmt = std.fmt;
 
-pub fn StringInitOptions(comptime T: type) type {
-    return struct {
-        initial_capacity: ?usize = null,
-    };
-}
+const StringInitOptions = struct {
+    initial_capacity: ?usize = null,
+};
 
 pub fn String(comptime T: type) type {
     // @TODO: Figure out if a way of calculating new capacity is general enough where it should be
@@ -27,6 +25,7 @@ pub fn String(comptime T: type) type {
 
         allocator: *mem.Allocator,
         capacity: usize,
+        count: usize,
 
         // @NOTE: Regard as private
         __chars: []T,
@@ -36,12 +35,17 @@ pub fn String(comptime T: type) type {
         /// The caller is responsible for calling `successful_return_value.deinit()`.
         pub fn init(
             allocator: *mem.Allocator,
-            options: StringInitOptions(T),
+            options: StringInitOptions,
         ) !String(T) {
             const capacity = if (options.initial_capacity) |c| c else 0;
-            var chars = if (options.initial_capacity) |c| try allocator.alloc(T, c) else [_]T{};
+            var chars = try allocator.alloc(T, capacity);
 
-            return Self{ .__chars = chars, .capacity = capacity, .allocator = allocator };
+            return Self{
+                .__chars = chars,
+                .capacity = capacity,
+                .allocator = allocator,
+                .count = 0,
+            };
         }
 
         /// Deinitializes the string, clearing all values inside of it and freeing the memory used.
@@ -49,6 +53,7 @@ pub fn String(comptime T: type) type {
             self.allocator.free(self.__chars);
             self.__chars = [_]T{};
             self.capacity = 0;
+            self.count = 0;
         }
 
         /// Copies a const slice of type `T`, meaning this can be used to create a string from a
@@ -61,42 +66,55 @@ pub fn String(comptime T: type) type {
                 .__chars = chars,
                 .capacity = slice.len,
                 .allocator = allocator,
+                .count = slice.len,
             };
         }
 
         /// Appends a `[]const T` onto a `String(T)`, mutating the appended to string. The allocator
         /// remains the same as the appended to string and the caller is not the owner of the memory.
+        /// If the current capacity of the string is enough to hold the new string no new memory
+        /// will be allocated.
         pub fn append(self: *Self, slice: ConstSlice) !void {
-            const new_capacity = self.capacity + slice.len;
-            var chars = try self.allocator.realloc(self.__chars, new_capacity);
-            mem.copy(T, chars[self.capacity..], slice);
+            const new_capacity = self.getNewCapacity(slice);
+            var chars = self.__chars;
+            if (new_capacity > self.capacity) {
+                chars = try self.allocator.realloc(self.__chars, new_capacity);
+            }
+            mem.copy(T, chars[self.count..], slice);
             self.__chars = chars;
             self.capacity = new_capacity;
+            self.count = self.count + slice.len;
         }
 
-        /// Appends a `[]const T` onto a `String(T)`, creating a new `String(T)` from it.
+        /// Appends a `[]const T` onto a `String(T)`, creating a new `String(T)` from it. The copy
+        /// will ignore the capacity of the original string.
         /// The caller is responsible for calling `successful_return_value.deinit()`.
         pub fn appendCopy(self: Self, allocator: *mem.Allocator, slice: ConstSlice) !Self {
-            var chars = try mem.concat(allocator, T, [_][]const T{ self.__chars, slice });
+            var chars = try mem.concat(
+                allocator,
+                T,
+                [_][]const T{ self.__chars[0..self.count], slice },
+            );
 
             return Self{
                 .__chars = chars,
                 .capacity = chars.len,
                 .allocator = allocator,
+                .count = chars.len,
             };
         }
 
         /// Returns a mutable copy of the contents of the `String(T)`.
         /// The caller is responsible for calling `successful_return_value.deinit()`.
         pub fn sliceCopy(self: Self, allocator: *mem.Allocator) !Slice {
-            var chars = try mem.dupe(allocator, T, self.__chars);
+            var chars = try mem.dupe(allocator, T, self.__chars[0..self.count]);
 
             return chars;
         }
 
         /// Returns an immutable copy of the contents of the `String(T)`.
         pub fn sliceConst(self: Self) ConstSlice {
-            return self.__chars;
+            return self.__chars[0..self.count];
         }
 
         /// Creates a `String(T)` from a format string.
@@ -109,7 +127,12 @@ pub fn String(comptime T: type) type {
         ) !Self {
             const chars = try fmt.allocPrint(direct_allocator, format_string, args);
             const capacity = chars.len;
-            return Self{ .__chars = chars, .capacity = capacity, .allocator = allocator };
+            return Self{
+                .__chars = chars,
+                .capacity = capacity,
+                .allocator = allocator,
+                .count = chars.len,
+            };
         }
 
         pub fn format(
@@ -120,9 +143,44 @@ pub fn String(comptime T: type) type {
             comptime Errors: type,
             output: fn (@typeOf(context), []const u8) Errors!void,
         ) Errors!void {
-            return fmt.format(context, Errors, output, "{}", self.__chars);
+            return fmt.format(context, Errors, output, "{}", self.__chars[0..self.count]);
+        }
+
+        fn getNewCapacity(self: Self, slice: ConstSlice) usize {
+            return max(@typeOf(self.capacity), self.capacity, self.count + slice.len);
         }
     };
+}
+
+test "`append` with an already high enough capacity doesn't change capacity" {
+    const initial_capacity = 80;
+    var string = try String(u8).init(direct_allocator, StringInitOptions{
+        .initial_capacity = initial_capacity,
+    });
+
+    testing.expectEqual(string.count, 0);
+    testing.expectEqual(string.capacity, initial_capacity);
+
+    const added_slice = "hello";
+    try string.append(added_slice);
+    testing.expectEqual(string.capacity, initial_capacity);
+    testing.expectEqual(string.count, added_slice.len);
+}
+
+test "`appendCopy` doesn't bring unused space with it" {
+    const initial_capacity = 80;
+    var string = try String(u8).init(direct_allocator, StringInitOptions{
+        .initial_capacity = initial_capacity,
+    });
+
+    testing.expectEqual(string.count, 0);
+    testing.expectEqual(string.capacity, initial_capacity);
+
+    const added_slice = "hello";
+    const string2 = try string.appendCopy(direct_allocator, added_slice);
+    testing.expectEqual(string2.capacity, added_slice.len);
+    testing.expectEqual(string2.count, added_slice.len);
+    testing.expectEqualSlices(u8, string2.sliceConst(), added_slice);
 }
 
 test "`appendCopy` doesn't disturb original string, `copyConst` copies static strings" {
@@ -135,6 +193,7 @@ test "`appendCopy` doesn't disturb original string, `copyConst` copies static st
         string2.sliceConst(),
         string3.__chars[0..string2.sliceConst().len],
     );
+    testing.expectEqualSlices(u8, string2.sliceConst(), "hello there");
     testing.expect(&string2.sliceConst()[0] != &string3.sliceConst()[0]);
     testing.expect(string3.capacity == 14);
     string3.deinit();
@@ -173,4 +232,9 @@ test "`fromFormat` returns a correct `String`" {
     testing.expectEqualSlices(u8, string_from_format.sliceConst(), expected_chars);
     testing.expectEqual(string_from_format.capacity, expected_capacity);
     testing.expectEqual(string_from_format.allocator, direct_allocator);
+}
+
+// This probably exists somewhere but I'm not wasting time looking for it
+fn max(comptime T: type, a: T, b: T) T {
+    return if (a > b) a else b;
 }
