d1148d31e2c95489a8f7699e4ec9de27f5126390
Rickard Andersson <gonz@severnatazvezda.com>
2019-11-30 18:54:37 +0200

    add test for `s` without range

--- src/vim.zig
+++ src/vim.zig
@@ -2182,6 +2182,35 @@ test "`igaf%C-[` = 'enter insert mode, then exit it'" {
     testing.expect(std.meta.activeTag(last_command) == Command.ExitInsertMode);
 }
 
+test "`sgaf%C-[` = 'replace current character, then exit insert mode'" {
+    const input = "sgaf%\x1b";
+    const commands = try parseInput(direct_allocator, input);
+    testing.expectEqual(commands.count(), 6);
+    const command_slice = commands.toSliceConst();
+    const first_command = command_slice[0];
+    testing.expect(std.meta.activeTag(first_command) == Command.ReplaceInsert);
+    switch (first_command) {
+        .ReplaceInsert => |command_data| {
+            testing.expectEqual(command_data.range, 1);
+            testing.expectEqual(command_data.register, null);
+        },
+        else => unreachable,
+    }
+    const insert_commands = command_slice[1..(command_slice.len - 1)];
+    for (insert_commands) |ic, index| {
+        testing.expect(std.meta.activeTag(ic) == Command.Insert);
+        switch (ic) {
+            .Insert => |character| {
+                // +2 because of `3s`
+                testing.expectEqual(character, input[index + 1]);
+            },
+            else => unreachable,
+        }
+    }
+    const last_command = command_slice[5];
+    testing.expect(std.meta.activeTag(last_command) == Command.ExitInsertMode);
+}
+
 test "`3sgaf%C-[` = 'replace three characters, then exit insert mode'" {
     const input = "3sgaf%\x1b";
     const commands = try parseInput(direct_allocator, input);
