8a8a85d9dbdc93977b6d81ed7d3592881d315b08
Rickard Andersson <gonz@severnatazvezda.com>
2019-12-03 21:12:22 +0200

    move tests out to `vim_tests.zig`

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,9 @@
 pub const string = @import("./string.zig");
 pub const file_buffer = @import("./file_buffer.zig");
-pub const vim = @import("./vim.zig");
+pub const vim_tests = @import("./vim_tests.zig");
 
 test "run tests" {
     string.runTests();
     file_buffer.runTests();
-    vim.runTests();
+    vim_tests.runTests();
 }
