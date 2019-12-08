7de2b49d983219ac12a2ace1bb07d38e7d6b862c
Rickard Andersson <gonz@severnatazvezda.com>
2019-12-08 17:22:28 +0200

    add `vim_state` to `BufferState`

--- src/buffer_state.zig
+++ src/buffer_state.zig
@@ -1,6 +1,7 @@
 const file_buffer = @import("./file_buffer.zig");
 const FileBuffer = @import("./file_buffer.zig").FileBuffer;
 const FileBufferOptions = @import("./file_buffer.zig").FileBufferOptions;
+const vim = @import("./vim.zig");
 
 const std = @import("std");
 const mem = std.mem;
@@ -10,11 +11,15 @@ pub fn BufferState(comptime T: type) type {
         const Self = @This();
 
         buffer: FileBuffer(T),
+        vim_state: vim.State,
 
         pub fn init(allocator: *mem.Allocator) !Self {
             var buffer = try FileBuffer(T).init(allocator, FileBufferOptions{});
 
-            return Self{ .buffer = buffer };
+            return Self{
+                .buffer = buffer,
+                .vim_state = vim.State.start(),
+            };
         }
     };
 }
--- src/buffer_state_tests.zig
+++ src/buffer_state_tests.zig
@@ -2,7 +2,10 @@ const BufferState = @import("./buffer_state.zig").BufferState;
 
 const std = @import("std");
 const direct_allocator = std.heap.direct_allocator;
+const testing = std.testing;
+const meta = std.meta;
 
 test "can create `BufferState`" {
     var buffer_state = try BufferState(u8).init(direct_allocator);
+    testing.expect(meta.activeTag(buffer_state.vim_state) == .Start);
 }
